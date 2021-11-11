0d47a1dc6fe5c476a822638cb95a13c0c59d04d1
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-11-11 17:18:40 +0100

    START: importati file necessari, creata navbar header con ciclo v-for

---
+++ src/components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+  <header>
+    <div>
+      <img src="../assets/dc-logo.png" alt="dc-logo" />
+    </div>
+    <div>
+      <ul>
+        <li v-for="(element, i) in headerNavList" :key="i">
+          <a :href="element.src">{{ element.txt }}</a>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      headerNavList: [
+        {
+          txt: "CHARACTERS",
+          src: "#",
+        },
+        {
+          txt: "COMICS",
+          src: "#",
+        },
+        {
+          txt: "MOVIES",
+          src: "#",
+        },
+        {
+          txt: "TV",
+          src: "#",
+        },
+        {
+          txt: "GAMES",
+          src: "#",
+        },
+        {
+          txt: "COLLECTIBLES",
+          src: "#",
+        },
+        {
+          txt: "VIDEOS",
+          src: "#",
+        },
+        {
+          txt: "FANS",
+          src: "#",
+        },
+        {
+          txt: "NEWS",
+          src: "#",
+        },
+        {
+          txt: "SHOP",
+          src: "#",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../styles/header";
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <div>
+      <h3>Content goes here</h3>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../styles/main";
+</style>
